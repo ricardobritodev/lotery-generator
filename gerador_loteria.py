fc835adf293388f01f2c113c4b38bea072968b98
Ricardo Brito <r.cardobrito@gmail.com>
2025-05-06 11:31:10 -0300

    Adcionado mais duas funções de geradores. QUINA e LOTOMANIA.

--- gerador_loteria.py
+++ gerador_loteria.py
@@ -53,3 +53,55 @@ def gerador_lotofacil():
 
     print("===" * 8, "Boa sorte!", "===" * 8)
     return resultado
+
+def gerador_quina():
+    print("=====" * 16)
+    print("===" * 8, "  GERADOR DE APOSTA QUINA  ", "===" * 8)
+    print("=====" * 16)
+
+    numero_jogos = int(input(">> Digite o número de jogos: "))
+    jogo = 0
+    resultado = []
+
+    for linha in range(int(numero_jogos)):
+        jogo += 1
+        numeros = random.sample(range(1, 81), 5)
+
+        for coluna in range(1):
+            numeros = random.sample(range(1, 81), 5)
+            print(f'>> Jogo {jogo}: ')
+            print(numeros, end='')
+
+        print()
+        print("---" * 20)
+        time.sleep(1)
+        resultado.append(numeros)
+
+    print("===" * 8, "Boa sorte!", "===" * 8)
+    return resultado
+
+def gerador_lotomania():
+    print("=====" * 16)
+    print("===" * 8, "  GERADOR DE APOSTA LOTOMANIA  ", "===" * 8)
+    print("=====" * 16)
+
+    numero_jogos = int(input(">> Digite o número de jogos: "))
+    jogo = 0
+    resultado = []
+
+    for linha in range(int(numero_jogos)):
+        jogo += 1
+        numeros = random.sample(range(0, 100), 20)
+
+        for coluna in range(1):
+            numeros = random.sample(range(0, 100), 20)
+            print(f'>> Jogo {jogo}: ')
+            print(numeros, end='')
+
+        print()
+        print("---" * 20)
+        time.sleep(1)
+        resultado.append(numeros)
+
+    print("===" * 8, "Boa sorte!", "===" * 8)
+    return resultado
--- main.py
+++ main.py
@@ -1,22 +1,21 @@
 
-from gerador_loteria import gerador_megasena
-from gerador_loteria import gerador_lotofacil
+from gerador_loteria import gerador_megasena, gerador_lotofacil, gerador_quina, gerador_lotomania
 
 print("=====" * 16)
 print("===" * 8, "  GERADOR DE APOSTA LOTERIA  ", "===" * 8)
 print("=====" * 16)
 
 print(">> QUAL JOGO DESEJA APOSTAR?")
-jogo = int(input("[1] MEGASENA]\n[2] LOTOFÁCIL]\n[3] LOTOMANIA]\n[4] DUPLASENA]\n>> "))
+jogo = int(input("[1] MEGASENA]\n[2] LOTOFÁCIL]\n[3] QUINA]\n[4] LOTOMANIA]\n>> "))
 
 if jogo == 1:
   gerador_megasena()
 elif jogo == 2:
   gerador_lotofacil()
 elif jogo == 3:
-  gerador_lotomania()
+  gerador_quina()
 elif jogo == 4:
-  gerador_duplasena()
+  gerador_lotomania()
 else:
   print("!!! ERRO, OPÇÃO INVALIDA !!! ")
 
